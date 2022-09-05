a09ef75e3bad771edb26b2787f4dfc091ef088cd
Namhoon Jeong <zoa0945@gmail.com>
2022-09-05 22:39:03 +0900

    feat: add profiletableview

--- MyLittleMovie/Scene/Subcomponents/ProfileView/ProfileViewController.swift
+++ MyLittleMovie/Scene/Subcomponents/ProfileView/ProfileViewController.swift
@@ -6,15 +6,57 @@
 //
 
 import UIKit
+import SnapKit
 
 class ProfileViewController: UIViewController {
+    
+    lazy var profileTableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(ProfileCell.self, forCellReuseIdentifier: "ProfileCell")
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        layout()
         view.backgroundColor = .systemBackground
         
         navigationItem.title = "User Name"
         navigationItem.largeTitleDisplayMode = .always
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as? ProfileCell else { return UITableViewCell() }
+        
+        return cell
+    }
+}
+
+extension ProfileViewController {
+    private func layout() {
+        view.addSubview(profileTableView)
+        
+        profileTableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
